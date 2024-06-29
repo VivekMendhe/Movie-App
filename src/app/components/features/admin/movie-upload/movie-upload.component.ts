f80a0cc7142aa4a9d04b5ae5c1c9d80c3e1ec5e1
Vivekanand Mendhe <vivek.mendhe.022@gmail.com>
2024-06-29 11:40:33 +0530

    Initial Commit

---
+++ src/app/components/features/admin/movie-upload/movie-upload.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-upload',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-upload.component.html',
+  styleUrl: './movie-upload.component.css'
+})
+export class MovieUploadComponent {
+
+}
